06642c65010bb84d47e783e320630b2bea436c35
Claes Ö <>
2021-05-01 10:59:10 +0200

    1.3.0

--- PluginBootstrap4Fs_modal.php
+++ PluginBootstrap4Fs_modal.php
@@ -1,15 +1,7 @@
 <?php
 class PluginBootstrap4Fs_modal{
-  public static function widget_include($data){
-    $element = array();
-    /**
-     * Library
-     */
-    $element[] = wfDocument::createHtmlElement('link', null,   array('href' => '/plugin/bootstrap4/fs_modal/dist/css/bootstrap-fs-modal.css', 'rel' => 'stylesheet'));
-    /**
-     * Fix
-     */
-    $element[] = wfDocument::createHtmlElement('link', null,   array('href' => '/plugin/bootstrap4/fs_modal/style.css', 'rel' => 'stylesheet'));
-    wfDocument::renderElement($element);
+  public static function widget_include(){
+    wfPlugin::enable('include/js');
+    wfDocument::renderElementFromFolder(__DIR__, __FUNCTION__);
   }
 }
